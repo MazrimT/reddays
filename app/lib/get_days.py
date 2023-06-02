f4b20b4f8637597513eb882eacf4b32cf37c92a3
unknown <gabriel.grunberg@gmail.com>
2023-06-02 11:00:35 +0200

    feat: cleanup

--- app/lib/get_days.py
+++
@@ -1,8 +0,0 @@
-import requests
-import json
-
-def get_days(year, all_days=False):
-    base_url = 'http://sholiday.faboul.se/dagar/v2.1/'
-    days = json.loads(requests.get(f"{base_url}{year}").text)['dagar']
-
-    return [day for day in days if int(day['dag i vecka']) < 6 and day['arbetsfri dag'] == 'Ja']
