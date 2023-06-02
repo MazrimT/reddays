63374f8d325bb0fabe1255da3db070eb5ff70a77
unknown <gabriel.grunberg@gmail.com>
2023-06-02 09:16:55 +0200

    feat: hello world

--- run.py
+++ run.py
@@ -1,21 +1,4 @@
-import subprocess
-import os
-from pathlib import Path
-
-def run():
-    """ runs the app.py file within the set up virtual environment
-    """
-
-    # find the base path and set path to python binary
-    base_path = Path(__file__).parent
-    python_bin = f"{base_path}/venv/Scripts/python" if os.name == 'nt' else f"{base_path}/venv/bin/python3"
-
-    cmd = f"{python_bin} app.py"
-    process = subprocess.Popen(cmd, shell=True) 
-    process.wait()
-    # kill the process
-    process.kill()
-
+from app.main import app
 
 if __name__ == '__main__':
-    run()    
+    app.run
