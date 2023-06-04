0e3be409fc28e754d3d1ac866eace8a211332741
unknown <gabriel.grunberg@gmail.com>
2023-06-04 02:10:28 +0200

    feat: checkbox to show weekend days or not

--- app/views/index.py
+++ app/views/index.py
@@ -8,11 +8,28 @@ from datetime import datetime
 
 index = Blueprint('index', __name__)
 
-def get_days(year, all_days=False):
+def get_days(year, show_weekend=False):
     base_url = 'http://sholiday.faboul.se/dagar/v2.1/'
     days = json.loads(requests.get(f"{base_url}{year}").text)['dagar']
 
-    return [day for day in days if int(day['dag i vecka']) < 6 and day['arbetsfri dag'] == 'Ja']
+    days = [
+        {
+            "date": day['datum'],
+            "red": True if day['röd dag'] == 'Ja' else False,
+            "workfree": True if day['arbetsfri dag'] == 'Ja' else False,
+            "holiday": day['helgdag'] if "helgdag" in day else "",
+            "day_of_week": int(day['dag i vecka']),
+            "weekday": day['veckodag'],
+            "week": day['vecka']
+        }
+        for day in days
+    ]
+
+ 
+    if show_weekend:
+        return [day for day in days if (day['day_of_week'] < 6 and day['workfree']) or (day['red'] and day["holiday"])]
+    else:
+        return [day for day in days if day['day_of_week'] < 6 and day['workfree']]
 
 
 @index.route('/', methods=["POST", "GET"])
@@ -23,7 +40,10 @@ def v_index():
  
     year = current_year if request.method == 'GET' else request.form["year"] 
 
-    days = get_days(year)
+    show_weekend = False if request.method == 'GET' or (request.method == 'POST' and "checkbox" in request.form.keys()) else True
+    days = get_days(year, show_weekend)
 
-    return render_template('index.html', days=days, default_year=year, current_year=current_year, years=years)
+    has_none_red = True if False in [day['red'] for day in days] else False
+    
+    return render_template('index.html', days=days, default_year=year, current_year=current_year, years=years, show_weekend=show_weekend, has_none_red=has_none_red)
     
